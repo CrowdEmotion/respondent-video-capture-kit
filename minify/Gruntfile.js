1691d75d5f9ab78ac748c65781c798935585b70c
stecrv <stefano.caravana.service@gmail.com>
2014-12-07 14:43:43 +0000

    [#83301486] Update VRTK to receive user id and stimulus id from 3rd party path added and documentation

--- minify/Gruntfile.js
+++ minify/Gruntfile.js
@@ -13,9 +13,16 @@ module.exports = function(grunt) {
                     mangle : false,
                     compress: false
                 },
-                src: ['../flash/js/external/swfobject.js','../flash/js/recorder/webproducer.js','../flash/js/APIClient/sha256.js','../flash/js/APIClient/enc-base64-min.js'
-                , '../flash/js/APIClient/store.js','../flash/js/APIClient/CEClient.js','../flash/js/video/video-js-4-7/video.dev.js'
-                , '../flash/js/video_respondent_test/videoRespondentTestPlayer.js', '../flash/js/video_respondent_test/videoRespondentTest.js'],
+                src: ['../flash/js/external/swfobject.js',
+                    '../flash/js/recorder/webproducer.js',
+                    '../flash/js/APIClient/sha256.js',
+                    '../flash/js/APIClient/enc-base64-min.js'
+                , '../flash/js/APIClient/store.js',
+                    '../flash/js/APIClient/CEClient.js',
+                    '../flash/js/video/video-js-4-7/video.dev.js'
+                , '../flash/js/video_respondent_test/videoRespondentTestPlayer.js',
+                    '../flash/js/video_respondent_test/videoRespondentTest.js'],
+
                 dest: '../flash/js/vrtk.min.js'
             }
         }
