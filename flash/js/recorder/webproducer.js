6c449cb91a00fb542da881981b518edd17723af0
stecrv <stefano.caravana.service@gmail.com>
2015-01-02 10:57:56 +0100

    [#84580362] webproducer update webpr js + swf files in webapp and playcorder

--- flash/js/recorder/webproducer.js
+++ flash/js/recorder/webproducer.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 var WebProducer = function (options) {
   if (!options || !options.id) {
     return alert('You must provide an id for the web producer');
@@ -61,6 +60,7 @@ WebProducer.extend = function (source) {
 
 WebProducer.prototype = {
   createElement: function (id, width, height, path) {
+    var self = this;
     var swfVersionStr = "11.4.0";
     var xiSwfUrlStr = "playerProductInstall.swf";
     var flashvars = { id: id };
@@ -71,18 +71,27 @@ WebProducer.prototype = {
     params.allowfullscreen = "true";
     var attributes = {};
     attributes.align = "left";
+    var check_already_ready = function() { self.check_already_ready(); };
+    this.on('ready', function () {
+      self.on_ready.apply(self, arguments);
+    });
     swfobject.embedSWF(
         path + "producer.swf", id,
         width, height,
         swfVersionStr, xiSwfUrlStr, 
-        flashvars, params, attributes);
+        flashvars, params, attributes, check_already_ready);
     // JavaScript enabled so display the flashContent div in case it is not replaced with a swf object.
     swfobject.createCSS("#"+id, "display:block;text-align:left;");
-    
+  },
+
+  check_already_ready: function () {
     var self = this;
-    this.on('ready', function () {
-      self.on_ready.apply(self, arguments);
-    });
+    try {
+      var el = document.getElementById(self.id);
+      if (el && el.isReady()) {
+        self.fire('ready');
+      }
+    } catch (e) {}
   },
 
   get_http_base_url: function () {
@@ -132,9 +141,9 @@ WebProducer.prototype = {
   },
 
   on_publish: function (streamName) {
-    this.publishStartTime = Date.now();
+    this.publishStartTime = (new Date()).getTime();
     this.streamName = streamName;
-  },
+  }
 };
 
 
@@ -203,7 +212,7 @@ var ContentsMixin = {
     jQuery.ajax({
       url : url, dataType: 'jsonp'
     }).then(cb);
-  },
+  }
 };
 
 WebProducer.extend(ContentsMixin);
@@ -216,7 +225,7 @@ var TimedMetadataMixin = {
       this.get_http_api_base_url(), 'timedmetadata/', this.streamName, '/append'
     ].join('');
     var data = metadata;
-    data.ts = data.ts || Date.now() - this.publishStartTime;
+    data.ts = data.ts || (new Date()).getTime() - this.publishStartTime;
     var dfr = jQuery.ajax({
       url : url,
       dataType: 'json',
@@ -232,7 +241,7 @@ var TimedMetadataMixin = {
       this.get_http_api_base_url(), 'timedmetadata/', this.streamName, '/append/jsonp'
     ].join('');
     var data = metadata;
-    data.ts = data.ts || Date.now() - this.publishStartTime;
+    data.ts = data.ts || (new Date()).getTime() - this.publishStartTime;
     data = "data=" + encodeURIComponent(JSON.stringify(data));
 
     var dfr = new $.Deferred();
@@ -260,7 +269,7 @@ var TimedMetadataMixin = {
       return;
     }
     this.addTimedMetadataJSONP(metadata, success, error);
-  },
+  }
 };
 
 WebProducer.extend(TimedMetadataMixin);
@@ -296,7 +305,7 @@ var JobsMixin = {
       type: 'get'
     }).fail(dfr_error).fail(error).done(dfr_done).done(success);
     return dfr;
-  },
+  }
 
 };
 
@@ -338,3 +347,107 @@ var EventEmitterMixin = {
 };
 
 WebProducer.extend(EventEmitterMixin);
+
+
+// Polyfill for Array.prototype.forEach for IE 8 
+// as seen at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function(callback, thisArg) {
+    var T, k;
+    if (this == null) {
+      throw new TypeError(' this is null or not defined');
+    }
+    // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+    var O = Object(this);
+    // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + ' is not a function');
+    }
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+      T = thisArg;
+    }
+    // 6. Let k be 0
+    k = 0;
+    // 7. Repeat, while k < len
+    while (k < len) {
+      var kValue;
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+        // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+        kValue = O[k];
+        // ii. Call the Call internal method of callback with T as the this value and
+        // argument list containing kValue, k, and O.
+        callback.call(T, kValue, k, O);
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined
+  };
+}
+
+// Polyfill for Array.prototype.indexOf for IE 8 
+// as seen at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(searchElement, fromIndex) {
+    var k;
+    // 1. Let O be the result of calling ToObject passing
+    //    the this value as the argument.
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+    var O = Object(this);
+    // 2. Let lenValue be the result of calling the Get
+    //    internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
+    // 4. If len is 0, return -1.
+    if (len === 0) {
+      return -1;
+    }
+    // 5. If argument fromIndex was passed let n be
+    //    ToInteger(fromIndex); else let n be 0.
+    var n = +fromIndex || 0;
+    if (Math.abs(n) === Infinity) {
+      n = 0;
+    }
+    // 6. If n >= len, return -1.
+    if (n >= len) {
+      return -1;
+    }
+    // 7. If n >= 0, then Let k be n.
+    // 8. Else, n<0, Let k be len - abs(n).
+    //    If k is less than 0, then let k be 0.
+    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+    // 9. Repeat, while k < len
+    while (k < len) {
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the
+      //    HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      //    i.  Let elementK be the result of calling the Get
+      //        internal method of O with the argument ToString(k).
+      //   ii.  Let same be the result of applying the
+      //        Strict Equality Comparison Algorithm to
+      //        searchElement and elementK.
+      //  iii.  If same is true, return k.
+      if (k in O && O[k] === searchElement) {
+        return k;
+      }
+      k++;
+    }
+    return -1;
+  };
+}
+
