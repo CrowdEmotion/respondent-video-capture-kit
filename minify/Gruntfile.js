230ce0229f23c8fdf0462f327bfbed1cbb856b66
Davide Bertola <dade@dadeb.it>
2015-06-10 20:02:48 +0200

    improve gruntfile formatting

--- minify/Gruntfile.js
+++ minify/Gruntfile.js
@@ -12,45 +12,47 @@ module.exports = function(grunt) {
         uglify : {
             default : {
                 options: {
-                    banner: '/* Playcorder crowdemotion.co.uk '+w+' */ ',
+                    banner: '/* Playcorder crowdemotion.co.uk ' + w + ' */ ',
                     mangle : false,
                     compress: false
                 },
-                src: ['../flash/js/external/swfobject.js',
+                src: [
+                    '../flash/js/external/swfobject.js',
                     '../flash/js/recorder/webproducer.js',
                     '../flash/js/recorder/browsercompatibility.js',
                     '../flash/js/APIClient/sha256.js',
-                    '../flash/js/APIClient/enc-base64-min.js'
-                , '../flash/js/APIClient/store.js',
+                    '../flash/js/APIClient/enc-base64-min.js',
+                    '../flash/js/APIClient/store.js',
                     '../flash/js/APIClient/CEClient.js',
-                    '../flash/js/video/video-js-4-7/video.dev.js'
-                , '../flash/js/video_respondent_test/videoRespondentTestPlayer.js',
-                    '../flash/js/video_respondent_test/videoRespondentTest.js'],
-
+                    '../flash/js/video/video-js-4-7/video.dev.js',
+                    '../flash/js/video_respondent_test/videoRespondentTestPlayer.js',
+                    '../flash/js/video_respondent_test/videoRespondentTest.js'
+                ],
                 dest: '../flash/js/vrtk.min.js'
             },
             concat : {
                 options: {
-                    banner: '/* Playcorder crowdemotion.co.uk '+w+' */ ',
+                    banner: '/* Playcorder crowdemotion.co.uk ' + w + ' */ ',
                     mangle : false,
                     compress: false,
                     expand: true,
                     beautify: true
                 },
-                src: ['../flash/js/external/swfobject.js',
+                src: [
+                    '../flash/js/external/swfobject.js',
                     '../flash/js/recorder/webproducer.js',
                     '../flash/js/recorder/browsercompatibility.js',
                     '../flash/js/APIClient/sha256.js',
-                    '../flash/js/APIClient/enc-base64-min.js'
-                    , '../flash/js/APIClient/store.js',
+                    '../flash/js/APIClient/enc-base64-min.js',
+                    '../flash/js/APIClient/store.js',
                     '../flash/js/APIClient/CEClient.js',
-                    '../flash/js/video/video-js-4-7/video.dev.js'
-                    , '../flash/js/video_respondent_test/videoRespondentTestPlayer.js',
-                    '../flash/js/video_respondent_test/videoRespondentTest.js'],
+                    '../flash/js/video/video-js-4-7/video.dev.js',
+                    '../flash/js/video_respondent_test/videoRespondentTestPlayer.js',
+                    '../flash/js/video_respondent_test/videoRespondentTest.js'
+                ],
                 dest: '../flash/js/vrtk.all.js'
             }
         }
-
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
