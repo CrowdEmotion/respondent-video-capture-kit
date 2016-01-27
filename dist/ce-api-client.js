3de85ee3bdaa4dbbee6530d4a7df80afd3781f9f
stecrv <stefano.caravana@gmail.com>
2016-01-27 16:11:11 +0000

    minify

--- dist/ce-api-client.js
+++ dist/ce-api-client.js
@@ -1,4 +1,4 @@
-/* Javascript client crowdemotion.co.uk 2016-1-22 9:41 */ var CryptoJS = CryptoJS || function(i, p) {
+/* Javascript client crowdemotion.co.uk 2016-1-27 16:9 */ var CryptoJS = CryptoJS || function(i, p) {
     var f = {}, q = f.lib = {}, j = q.Base = function() {
         function a() {}
         return {
